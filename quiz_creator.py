7895ead491d49497759dfae4b0ec0ecdb8d72fe8
heidilyncaling <calingheidilyn@gmail.com>
2025-04-07 19:37:41 +0800

    Initial Commit: Uploading Algorithm

---
+++ quiz_creator.py
@@ -0,0 +1,11 @@
+#Welcome notes.
+
+#Loop:
+    #Input question
+    #Input choices (a, b, c, d)
+    #Input the correct answer.
+    #Save in txt file.
+    #Ask if input another or end program
+#If no, end loop.
+-
+#Show ending message.
