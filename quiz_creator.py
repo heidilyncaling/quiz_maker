d46b0504d567a1ea3041e99c7ea98b821a70981b
heidilyncaling <calingheidilyn@gmail.com>
2025-04-09 08:40:47 +0000

    Setting up display text and click to start function.

--- quiz_creator.py
+++ quiz_creator.py
@@ -1,13 +1,15 @@
-#Welcome notes.
+#Welcome notes
 import pygame
+import sys
 
 #initialize pygame
 pygame.init()
+pygame.mixer.init()
 
-#scree set up
+#screen set up
 WIDTH, HEIGHT = 800, 600
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Welcome to QUIZ CREATOR!")
+pygame.display.set_caption("QUIZ CREATOR GAME")
 
 #colors
 WHITE = (255, 255, 255)
@@ -15,16 +17,31 @@ PURPLE = (255, 0, 255)
 
 #font
 FONT = pygame.font.SysFont("Helvetica", 25)
+BIG_FONT = pygame.font.SysFont("Helovetica", 50)
+
+#center font in screen
+def draw_text(text, font, color, surface, x, y, center=True):
+    rendered = font.render(text, True, color)
+    rect = rendered.get_rect(center=(x,y)if center else(x,y))
+    surface.blit(rendered,rect)
 
 running = True
 while running:
     screen.fill(PURPLE)
+    draw_text("Welcome to Quiz Creator Game!", BIG_FONT, WHITE, screen, WIDTH // 2, HEIGHT // 3)
+    draw_text("Click to Start!", FONT, WHITE, screen, WIDTH // 2, HEIGHT // 1.5)
+
+    pygame.display.flip()
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    pygame.display.flip()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            running = False
 
 pygame.quit()
+sys.exit()
+
 #Loop:
     #Input question
     #Input choices (a, b, c, d)
